7a2dc6cd24771736eddb20a0138387acef003fe0
hushangni <shangnihu@gmail.com>
2018-05-12 00:24:45 -0400

    Finished responsiveness for mobile and tablet, added subscribe alert.

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-$('.hamburger-toggle').on('click', function(){
-    $(this).toggleClass('x-toggle');
-    $('nav').toggleClass('res-menu');
+$(document).ready( function() {
+	// Hamburger open close toggle top nav
+	$('.hamburger-toggle').on('click', function() {
+	    $(this).toggleClass('x-toggle');
+	    $('nav').toggleClass('res-menu');
+	});
+
+	// Happy subscribe sumit message
+	$('.submit').on('click', function() {
+		alert(`Thank you for subscribing to our newsletter!
+		\n (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ ✧ﾟ･: *ヽ(◕ヮ◕ヽ)
+		\n Fun things await your inbox!`);
+	});
 });
+
